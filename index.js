2624861ebc25f08587c8d32d0769a9f0dc1d0d42
Deepak Varshney <deepakvarshney.com@gmail.com>
2026-07-09 15:37:18 +0530

    added api prefix

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
+// import express from "express";
+// import dotenv from "dotenv"
+// dotenv.config();
+// const PORT = process.env.PORT || 5000
+// import authRoutes from "./src/routes/auth.routes.js"
+
+
+
+
+// const app = express();
+
+// app.use(express.json());
+// app.use('/auth', authRoutes);
+
+
+
+// app.get('/health', async (req, res) => {
+//     try {
+//         res.json("working");
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json(err);
+//     }
+// })
+
+// app.listen(PORT, () => {
+//     console.log(`Server running at http://localhost:${PORT}`)
+// })
+
+
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import authRoutes from "./src/routes/auth.routes.js";
+
 dotenv.config();
-const PORT = process.env.PORT || 5000
-import authRoutes from "./src/routes/auth.routes.js"
 
-
-
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
-app.use('/auth', authRoutes);
 
+// API routes
+app.use("/api/auth", authRoutes);
 
-
-app.get('/health', async (req, res) => {
-    try {
-        res.json("working");
-    } catch (err) {
-        console.error(err);
-        res.status(500).json(err);
-    }
-})
+app.get("/api/health", (req, res) => {
+  res.json("working");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`)
-})
-
+  console.log(`Server running at http://localhost:${PORT}`);
+});
