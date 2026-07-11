d002a7b183311cd314f7fc94ea545048da668850
Deepak Varshney <deepakvarshney.com@gmail.com>
2026-07-11 10:23:45 +0530

    config vercel cors in backend

--- index.js
+++ index.js
@@ -5,12 +5,34 @@ import productRoutes from "./src/routes/product.routes.js";
 import cartRoutes from "./src/routes/cart.routes.js";
 import cookieParser from "cookie-parser";
 import { createCart } from "./src/controllers/cart.controller.js";
+import cors from "cors"
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+
 const app = express();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://twirll-restaurant.vercel.app",
+];
+
+app.use(
+  cors({
+    origin(origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use(express.json());
